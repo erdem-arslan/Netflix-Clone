08f7cb4108ddc2b445dc5f93ad3c9e1d669bc132
Erdem ARSLAN <erdemarsl4n@gmail.com>
2023-08-11 17:55:15 +0300

    update

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -13,16 +13,15 @@ const AccountMenu:React.FC<AccountMenuProps> = ({
 }) => {
     if(!visible) return null;
 
-    const {data: user}=useCurrentUser();
-    const profile = user ? user[0] : null;
+    // const {data: user}=useCurrentUser();
+    // const profile = user ? user[0] : null;
     
-    console.log(user);
     return (
         <div className='bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex'>
             <div className='flex flex-col gap-3'>
                 <div className='px-3 group/item flex flex-row gap-3 items-center w-full'>
                     <img className='w-8 rounded-md' src="/images/default-blue.png" alt="profile" />
-                    <p className='text-white text-sm group-hover/item:underline'>{profile?.name}</p>
+                    <p className='text-white text-sm group-hover/item:underline'></p>
                 </div>
                 <hr className='bg-gray-600 border-0 h-px my-4'/>
                 <div onClick={()=>signOut()} className='px-3 text-center text-white text-sm hover:underline'>
